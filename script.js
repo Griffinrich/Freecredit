c445b9e15424213a665756188b6b9f5d39492c9f
Free <griffin168888@gmail.com>
2024-06-02 02:53:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function randomCredit() {
-    var prizes = [50, 100, 150, 200, 250, 300, 350];
-    var randomIndex = Math.floor(Math.random() * prizes.length);
-    var prize = prizes[randomIndex];
-    document.getElementById('prize').textContent = prize;
-    document.getElementById('result').style.display = 'block';
+function spinWheel() {
+  // สุ่มเลขรางวัล
+  var rewards = [50, 100, 150, 200, 250, 300, 350];
+  var rewardIndex = Math.floor(Math.random() * rewards.length);
+  var rewardAmount = rewards[rewardIndex];
+
+  // แสดงป๊อปอัพ
+  var popup = document.getElementById("popup");
+  var rewardAmountDisplay = document.getElementById("rewardAmount");
+  rewardAmountDisplay.textContent = rewardAmount;
+
+  popup.style.display = "block";
 }
 
-function claimPrize() {
-    var username = document.getElementById('username').value;
-    alert("ยินดีด้วย! คุณได้รับรางวัลเครดิตฟรีแล้ว ลิ้งค์: https://lin.ee/WXS8t3t");
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
 }
