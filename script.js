42a805d2eba04ae6ce6a7f9c4c3db7295f12f077
Free <griffin168888@gmail.com>
2024-06-02 03:43:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-document.getElementById('random-button').addEventListener('click', function() {
-  var rewards = [50, 100, 150, 200, 250, 300, 350];
-  var randomReward = rewards[Math.floor(Math.random() * rewards.length)];
-  document.getElementById('reward-amount').innerText = randomReward;
-  document.getElementById('popup').style.display = 'flex';
-});
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('random-button').addEventListener('click', function () {
+    var rewards = [50, 100, 150, 200, 250, 300, 350];
+    var randomReward = rewards[Math.floor(Math.random() * rewards.length)];
+    document.getElementById('reward-amount').innerText = randomReward;
+    document.getElementById('popup').style.display = 'flex';
+  });
 
-document.querySelector('.close').addEventListener('click', function() {
-  document.getElementById('popup').style.display = 'none';
-});
+  document.querySelector('.close').addEventListener('click', function () {
+    document.getElementById('popup').style.display = 'none';
+  });
 
-document.getElementById('username-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var username = document.getElementById('username').value;
-  if (username.trim() !== '') {
-    alert('ยินดีด้วย, ' + username + '! คุณได้รับรางวัลของคุณแล้ว!');
-    window.location.href = 'https://lin.ee/WXS8t3t';
-  } else {
-    alert('กรุณากรอกชื่อผู้ใช้ของคุณ');
-  }
+  document.getElementById('username-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    if (username.trim() !== '') {
+      alert('ยินดีด้วย, ' + username + '! คุณได้รับรางวัลของคุณแล้ว!');
+      window.location.href = 'https://lin.ee/WXS8t3t';
+    } else {
+      alert('กรุณากรอกชื่อผู้ใช้ของคุณ');
+    }
+  });
 });
